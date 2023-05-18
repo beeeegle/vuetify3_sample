3d85366a31378fe5667a009d79f4d0e38fc79d9e
beeeegle <aqua.dev.sphere@gmail.com>
2023-05-18 16:35:55 +0900

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vuetify3_sample/',
   plugins: [
     vue({
       template: { transformAssetUrls }
